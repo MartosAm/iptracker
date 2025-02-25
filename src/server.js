1b46402447dbd29f960c3fd7c1af86fa5cec3eb0
MartosAm <adrianvelarex@gmail.com>
2025-02-24 20:39:07 -0600

    probando el push
    push del console log

--- src/server.js
+++ src/server.js
@@ -23,3 +23,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+console.log("aver si jala ya estoy en la rama master y voy a hacer un push")
